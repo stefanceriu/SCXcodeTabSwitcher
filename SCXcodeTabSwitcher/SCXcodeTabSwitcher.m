61873694c844fadeb92c96fbaa99d3b668e9f206
Stefan Ceriu <stefan.ceriu@yahoo.com>
2015-05-31 19:51:11 +0100

    Added support for multiple workspaces and windows.

--- SCXcodeTabSwitcher/SCXcodeTabSwitcher.m
+++ SCXcodeTabSwitcher/SCXcodeTabSwitcher.m
@@ -11,11 +11,7 @@
 #import "IDESourceCodeEditor.h"
 #import "IDEWorkspaceWindowController.h"
 
-NSString *const IDESourceCodeEditorDidFinishSetupNotification = @"IDESourceCodeEditorDidFinishSetup";
-
 @interface SCXcodeTabSwitcher ()
-
-@property (nonatomic, weak) IDEWorkspaceWindowController *workspaceWindowController;
 
 @end
 
@@ -23,7 +19,7 @@ NSString *const IDESourceCodeEditorDidFinishSetupNotification = @"IDESourceCodeE
 
 + (void)pluginDidLoad:(NSBundle *)plugin
 {
-    static SCXcodeTabSwitcher *sharedPlugin;
+	static SCXcodeTabSwitcher *sharedPlugin;
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
 		sharedPlugin = [[self alloc] init];
@@ -32,57 +28,53 @@ NSString *const IDESourceCodeEditorDidFinishSetupNotification = @"IDESourceCodeE
 
 - (id)init
 {
-    if (self = [super init]) {
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onEditorDidFinishSetup:) name:IDESourceCodeEditorDidFinishSetupNotification object:nil];
-        
-        static NSDictionary *keysToTabsMapping;
-        static dispatch_once_t onceToken;
-        dispatch_once(&onceToken, ^{
-            keysToTabsMapping = @{@(18) : @(0),
-                                  @(19) : @(1),
-                                  @(20) : @(2),
-                                  @(21) : @(3),
-                                  @(23) : @(4),
-                                  @(22) : @(5),
-                                  @(26) : @(6),
-                                  @(28) : @(7),
-                                  @(25) : @(8)};
-        });
-        
-        [NSEvent addLocalMonitorForEventsMatchingMask:NSKeyDownMask handler:^NSEvent *(NSEvent *event) {
-            
-            NSEventModifierFlags flags = [event modifierFlags] & NSDeviceIndependentModifierFlagsMask;
-            if(flags == NSCommandKeyMask) {
-
-                NSNumber *tabNumber = keysToTabsMapping[@([event keyCode])];
-                if(!tabNumber) {
-                    return event;
-                } else {
-                    [self selectTabNumber:[tabNumber unsignedIntegerValue]];
-                    return nil;
-                }
-            }
-            
-            return event;
-        }];
-    }
-    
+	if (self = [super init]) {
+		
+		static NSDictionary *keysToTabsMapping;
+		static dispatch_once_t onceToken;
+		dispatch_once(&onceToken, ^{
+			keysToTabsMapping = @{@(18) : @(0),
+								  @(19) : @(1),
+								  @(20) : @(2),
+								  @(21) : @(3),
+								  @(23) : @(4),
+								  @(22) : @(5),
+								  @(26) : @(6),
+								  @(28) : @(7),
+								  @(25) : @(8)};
+		});
+		
+		[NSEvent addLocalMonitorForEventsMatchingMask:NSKeyDownMask handler:^NSEvent *(NSEvent *event) {
+			
+			NSEventModifierFlags flags = [event modifierFlags] & NSDeviceIndependentModifierFlagsMask;
+			if(flags == NSCommandKeyMask) {
+				
+				NSNumber *tabNumber = keysToTabsMapping[@([event keyCode])];
+				if(!tabNumber) {
+					return event;
+				} else {
+					[self selectTabNumber:[tabNumber unsignedIntegerValue]];
+					return nil;
+				}
+			}
+			
+			return event;
+		}];
+	}
+	
 	return self;
-}
-
-- (void)onEditorDidFinishSetup:(NSNotification*)sender
-{
-	self.workspaceWindowController = [[IDEWorkspaceWindowController workspaceWindowControllers] firstObject];
 }
 
 - (void)selectTabNumber:(NSUInteger)tabNumber
 {
-    if(tabNumber >= self.workspaceWindowController.tabView.numberOfTabViewItems) {
-        return;
-    }
-    
-    [self.workspaceWindowController _showTab:[self.workspaceWindowController.tabView tabViewItemAtIndex:tabNumber]];
+	NSWindow *keyWindow = [[NSApplication sharedApplication] keyWindow];
+	IDEWorkspaceWindowController *workspaceWindowController = [IDEWorkspaceWindowController workspaceWindowControllerForWindow:keyWindow];
+	
+	if(tabNumber >= workspaceWindowController.tabView.numberOfTabViewItems) {
+		return;
+	}
+	
+	[workspaceWindowController _showTab:[workspaceWindowController.tabView tabViewItemAtIndex:tabNumber]];
 }
 
 @end
